01c0cbfa55e5bc50f802b45bbf2e6ff98191189c
jackielu <jackielu@gmail.com>
2014-11-10 23:35:45 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Leaflet GeoJSON Example
+=======================
+
+Loads an external geoJSON file using $.getJSON, adds interactivity from elements outside of the map.   
+
+See it live here:  http://xsavi780.github.io/getjson 
+
---
+++ js/script.js
@@ -0,0 +1,49 @@
+
+//set up our map
+var map = L.map('map')
+	.setView([42.7559421,-75.8092041], 7);
+
+
+//set up basemap tiles from stamen
+L.tileLayer('http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20
+}).addTo(map);
+
+
+function makeMarkers(feature, layer){
+	//console.log(feature);
+	layer.bindPopup(
+		"Ecozone: <br>"
+		+ feature.properties.MINOR_DESC
+	);
+}
+
+$.getJSON('/data/ecozone_wgs84.geojson', function(data){
+	//console.log(data);
+	L.geoJson(data.features, {  //use leaflet's functionality to grab geoJSON features
+		onEachFeature: makeMarkers,
+		style: function(feature) {
+			switch (feature.properties.MAJORCOLOR) {
+				case 'ZONE A': return {color:"#a6cee3"};
+				case 'ZONE B': return {color:"#1f78b4"};
+				case 'ZONE C': return {color:"#b2df8a"};
+				case 'ZONE D': return {color:"#33a02c"};
+				case 'ZONE E': return {color:"#fb9a99"};
+				case 'ZONE F': return {color:"#e31a1c"};
+				case 'ZONE G': return {color:"#fdbf6f"};
+				case 'ZONE H': return {color:"#ff7f00"};
+				case 'ZONE I': return {color:"#cab2d6"};
+				case 'ZONE J': return {color:"#6a3d9a"};
+				case 'ZONE K': return {color:"#ffff99"};
+				case 'ZONE L': return {color:"#b15928"};
+			}
+		}, weight:0.75, opacity: 0.9, fillOpacity: 0.5
+	}).addTo(map);  //add to map
+});
+
+
+
+
